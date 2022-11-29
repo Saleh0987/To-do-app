ca64372fefa60ae07cb1f03d2b202e24c0a564c1
Saleh0987 <ms3617224@gmail.com>
2022-11-29 07:10:02 +0200

    add-readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # To-do-app
+
+## Add Tasks To Page
+
+## Remove Task From Page
+
+## Save Note In Local Storage
--- app.js
+++ app.js
@@ -106,10 +106,6 @@ function getDataFromLocalStorage() {
 }
 
 function deleteTaskWith(taskId) {
-  // For Explain Only
-  // for (let i = 0; i < arrayOfTasks.length; i++) {
-  //   console.log(`${arrayOfTasks[i].id} === ${taskId}`);
-  // }
   arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
   addDataToLocalStorageFrom(arrayOfTasks);
 }
